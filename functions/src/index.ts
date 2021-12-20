4c70acd6054a6abc1914a5cbe374971c380c44e1
Vincent Zhao <59675522+VincentZhao12@users.noreply.github.com>
2021-12-19 23:26:20 -0800

    firebase function for updating team data when docs are added

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,5 @@
-import * as functions from "firebase-functions";
+import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +8,68 @@ import * as functions from "firebase-functions";
 //   functions.logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp();
+const db = admin.firestore();
+
+export const matchUpdate = functions.firestore
+    .document('/years/{year}/regionals/{regional}/teams/{team}/matches/{match}')
+    .onCreate(async (snap, context) => {
+        const teamDoc = await db
+            .collection('years')
+            .doc(context.params.year)
+            .collection('regionals')
+            .doc(context.params.regional)
+            .collection('teams')
+            .doc(context.params.team)
+            .get();
+        const teamData = teamDoc.data() || {};
+        const matchData = snap.data();
+        let newTeamData: any = {};
+        let matches = (
+            await db
+                .collection('years')
+                .doc(context.params.year)
+                .collection('regionals')
+                .doc(context.params.regional)
+                .collection('teams')
+                .doc(context.params.team)
+                .collection('matches')
+                .get()
+        ).docs.length;
+        functions.logger.info(teamData);
+        Object.keys(matchData).forEach((key) => {
+            if (
+                key !== 'matchNum' &&
+                key !== 'teamNum' &&
+                typeof matchData[key] === 'number'
+            ) {
+                if (teamData[key]) {
+                    const value =
+                        teamData[key] * (matches - 1) + matchData[key];
+                    newTeamData[key] = value / matches;
+                } else {
+                    newTeamData[key] = matchData[key];
+                }
+            }
+            let endgamePoints = 5;
+            if (!matchData.hangFail) endgamePoints += 20;
+            if (!matchData.levelFail) endgamePoints += 15;
+            if (teamData.endgamePoints) {
+                newTeamData.endgamePoints =
+                    (teamData.endgamePoints * (matches - 1) + endgamePoints) /
+                    matches;
+            } else {
+                newTeamData.endgamePoints = endgamePoints;
+            }
+        });
+        newTeamData.matches = matches;
+        functions.logger.info(newTeamData);
+        db.collection('years')
+            .doc(context.params.year)
+            .collection('regionals')
+            .doc(context.params.regional)
+            .collection('teams')
+            .doc(context.params.team)
+            .set(newTeamData);
+    });
